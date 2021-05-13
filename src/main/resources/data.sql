7c6d67f8979407099ce6eced085d2a7e120f0639
Amanda Nguyen <anguyend@gmail.com>
2021-05-12 19:56:55 -0500

    title modification

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
-insert into items (sku, title, price) values ('abcr', 'All Butter Croissant', 0.75);
-insert into items (sku, title, price) values ('ccr', 'Chocolate Croissant', 0.95);
-insert into items (sku, title, price) values ('b', 'Fresh Baguette', 1.60);
-insert into items (sku, title, price) values ('rv', 'Red Velvet', 3.95);
-insert into items (sku, title, price) values ('vs', 'Victoria Sponge', 5.45);
-insert into items (sku, title, price) values ('cc', 'Carrot Cake', 3.45);
+insert into items (sku, title, price) values ('d1', 'Black Dachshund', 30);
+insert into items (sku, title, price) values ('c1', 'Corgi', 20);
+insert into items (sku, title, price) values ('d2', 'Brown Dachshund', 30);
+insert into items (sku, title, price) values ('d3', 'Spotted Dachshund', 35);
+insert into items (sku, title, price) values ('d4', 'Blonde Dachshund', 30);
+insert into items (sku, title, price) values ('d5', 'Red Dachshund', 35);
